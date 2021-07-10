1cb9e3e983f221aca1d0b9393df1ecc56f31a703
cloverspark <alexbrenna1999@hotmail.com>
2021-07-10 13:21:27 -0700

    labels set up

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,82 +29,82 @@ namespace TestinSouceControle
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.Lbcounter1 = new System.Windows.Forms.Label();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.Btn3 = new System.Windows.Forms.Button();
+            this.Lbcounter2 = new System.Windows.Forms.Label();
+            this.Lbcounter3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // Lbcounter1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(72, 61);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.Lbcounter1.AutoSize = true;
+            this.Lbcounter1.Location = new System.Drawing.Point(72, 61);
+            this.Lbcounter1.Name = "Lbcounter1";
+            this.Lbcounter1.Size = new System.Drawing.Size(18, 20);
+            this.Lbcounter1.TabIndex = 0;
+            this.Lbcounter1.Text = "0";
             // 
-            // button1
+            // Btn1
             // 
-            this.button1.Location = new System.Drawing.Point(76, 242);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 40);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Btn1.Location = new System.Drawing.Point(76, 241);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(101, 40);
+            this.Btn1.TabIndex = 1;
+            this.Btn1.Text = "add";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // Btn2
             // 
-            this.button2.Location = new System.Drawing.Point(288, 241);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 40);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Btn2.Location = new System.Drawing.Point(288, 241);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(101, 40);
+            this.Btn2.TabIndex = 2;
+            this.Btn2.Text = "add 1,2";
+            this.Btn2.UseVisualStyleBackColor = true;
+            this.Btn2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // Btn3
             // 
-            this.button3.Location = new System.Drawing.Point(503, 242);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(101, 40);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.Btn3.Location = new System.Drawing.Point(503, 242);
+            this.Btn3.Name = "Btn3";
+            this.Btn3.Size = new System.Drawing.Size(101, 40);
+            this.Btn3.TabIndex = 3;
+            this.Btn3.Text = "add all";
+            this.Btn3.UseVisualStyleBackColor = true;
+            this.Btn3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // label2
+            // Lbcounter2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(288, 61);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 20);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.Lbcounter2.AutoSize = true;
+            this.Lbcounter2.Location = new System.Drawing.Point(288, 61);
+            this.Lbcounter2.Name = "Lbcounter2";
+            this.Lbcounter2.Size = new System.Drawing.Size(18, 20);
+            this.Lbcounter2.TabIndex = 4;
+            this.Lbcounter2.Text = "0";
             // 
-            // label3
+            // Lbcounter3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(503, 61);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 20);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.Lbcounter3.AutoSize = true;
+            this.Lbcounter3.Location = new System.Drawing.Point(503, 61);
+            this.Lbcounter3.Name = "Lbcounter3";
+            this.Lbcounter3.Size = new System.Drawing.Size(18, 20);
+            this.Lbcounter3.TabIndex = 5;
+            this.Lbcounter3.Text = "0";
             // 
             // testing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lbcounter3);
+            this.Controls.Add(this.Lbcounter2);
+            this.Controls.Add(this.Btn3);
+            this.Controls.Add(this.Btn2);
+            this.Controls.Add(this.Btn1);
+            this.Controls.Add(this.Lbcounter1);
             this.Name = "testing";
             this.Text = "SourceControleTest";
             this.ResumeLayout(false);
@@ -114,12 +114,12 @@ namespace TestinSouceControle
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Lbcounter1;
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.Button Btn3;
+        private System.Windows.Forms.Label Lbcounter2;
+        private System.Windows.Forms.Label Lbcounter3;
     }
 }
 
